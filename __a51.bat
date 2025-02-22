52ebf5ad9eef525972e4c2e18a5cd7ad230d3b72
Randy Ren <randyren278@gmail.com>
2025-02-22 12:58:48 -0700

    compilation files for the alternate pwm file

---
+++ __a51.bat
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd "\Users\randy\developer\elec291-reflow\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "C:\Users\randy\developer\elec291-reflow\alternatepwm.asm"
+echo Crosside_Action Set_Hex_File C:\Users\randy\developer\elec291-reflow\alternatepwm.HEX
